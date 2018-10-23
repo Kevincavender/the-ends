a0330b46d4d22ce410e52c094e5330c00e116f39
Kevin <kevin.cavender@wsu.edu>
2018-10-22 20:16:58 -0700

    Create functions.py

---
+++ the_ends/functions.py
@@ -0,0 +1,29 @@
+'''
+Write function to identify functions being called
+
+example syntax:
+
+seperate "function("
+
+
+from:
+
+"x=2*5+function(x=x, 5, 12^(2-1))"
+
+example output:
+
+function_name = 'function'
+function_variables = {1:"x", 2: "5", 3: "12^(2-1)"}
+
+write as a python function with a single line string as the input
+
+Include a test case at bottom:
+
+2018-10-22
+Kevin Cavender
+Instructions for Sev on requested python function
+'''
+
+
+if __name__ == "__main__":
+  print("ASS")
