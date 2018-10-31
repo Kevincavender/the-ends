eb7a63cf751d799d745eb2f262d695f505ec5792
severnaa <44298139+severnaa@users.noreply.github.com>
2018-10-31 11:44:18 -0600

    Update the_ends/functions.py

--- the_ends/functions.py
+++ the_ends/functions.py
@@ -1,39 +1,11 @@
 import re
 
 '''
-PRoblems commiting to Git hub
-
-Write function to identify functions being called
-
-example syntax:
-
-separate "function("
-
-from:
-
-"x=2*5+function(x=x, 5, 12^(2-1))"
-
-example output (Updated 10/26/2018):
-
-return {'function_name': 'function', 1:"x", 2: "5", 3: "12^(2-1)"}
-
-write as a python function with a single line string as the input
-
-Include a test case at bottom:
-
-2018-10-22
-Kevin Cavender
-Instructions for Sev on requested python function
-
+Notes from Last Sev Update:
+    -Parenthese checking portion is used for the index so unless the error checking thing returns those I need that part
+    -We should look at what should be done with the integrated functions. As it is they will just be treated as variables and their variables will not be noted
 # TODO .... Maybe.....
 
--determine if a variable of a function is a unlisted function???
-    >>> yes, but first is adding a single function SQRT? and integrating it into everything else
--create a full program break for the related syntax errors
-    >>>This will be pushed more to the future
--replace .append method with marginally faster numpy.zeros() method
-    >>>staying with python standard library wherever possible (re is included in this) 
-    
 -run the_ends_test >>> FunctionsUnitTest.py
     >>> This will help us refine the functionality of this as we progress. 
         Makes is a pass/fail condition for whether things are as we both expect them to act.
@@ -47,35 +19,41 @@ ABS()
 2018-10-26 Kevin added Notes 
 '''
 
+integrated_functions_list = ['SQRT','ABS']
+
 
 def function_finder(equ, debug=False):
     """
-  Finds all functions and how many within a single string
-  Define name for each function and create a returnable list of names
-  """
-    fun_list1 = re.findall(r'\w+\(', equ, flags=0)
-    number_of_functions = len(fun_list1)
+    Finds all functions and how many within a single string
+    Define name for each function and create a returnable list of names
+    """
 
-    if number_of_functions == 0:
+    equ = equ.replace(' ', '')
+
+
+    fun_list = re.findall(r'\w+\(', equ, flags=0)
+    number_of_functions_ = len(fun_list)
+
+    if number_of_functions_ == 0:
         return [], []  # might want to change this depending on other functions
 
-    fun_list = []
-    for i in range(number_of_functions):
-        fun_list.append(fun_list1[i][:-1])
+    #removes trailing parenthese and removes from the list any function on the integrated function list.
+    #Add more here if you want to do something specific with them...
+    #Other wise they will appear as any other variable in a dictionary
+    for i in range(number_of_functions_):
+        fun_list[i] = fun_list[i][:-1]
+        if fun_list[i] in integrated_functions_list:
+            fun_list.remove(i)
 
+    number_of_functions_ = len(fun_list)
+    
     # figure out index of each open and close parentheses
-    # start of variable space
-    start_list = []
-    for j in range(number_of_functions):
-        start_list.append(re.search(re.escape(fun_list[j]), equ).end())
-        # TODO can these be combine to limit the number of looping sections?
 
     # figures out indexes of all parentheses
     parentheses_ranges = {}  # soon to be filled dictionary of ranges
     # .................................................................
     # TODO Replace this section with EquationErrorCheck.EquationErrorCheck()
-    # Should be good practice dealing with multiple files
-    # ____________________________________________________________
+    # we can replace if you want to make the parentheses_ranges a returnable for the equation checking function
     hold = []  #
     for i, c in enumerate(equ):
         if c == '(':
@@ -89,9 +67,11 @@ def function_finder(equ, debug=False):
     if hold:  # check if stack is empty afterwards
         # print('Syntax Error: Too many opening parentheses')
         raise SyntaxError
-    # ..................................................................
+
+    start_list = [] #starting points for function variable ranges
     function_ranges = []  # indices of only the functions
     for k in range(number_of_functions):
+        start_list.append(re.search(re.escape(fun_list[k]), equ).end())
         function_ranges.append((start_list[k], parentheses_ranges[start_list[k]]))
 
     # extract string, parse and return a dictionary of variables for each function
@@ -99,24 +79,19 @@ def function_finder(equ, debug=False):
     for n in range(number_of_functions):
         extracted_string = equ[function_ranges[n][0] + 1:function_ranges[n][1]]  # extracts string
 
-        # removes spaces. might be obsolete
-        # will already be done
-        # TODO remove this, it should be done before the string gets to this function
-        # extracted_string = og_string.replace(' ', '')
 
         # if statement about functions inside functions. remove to make good program
         # why are you looping to find errors?
         for m in range(number_of_functions):
             if fun_list[m] in extracted_string:
-                print('Syntax Error: Nested Function')
+                print('Syntax Error: Nested Non-Integrated Function')
                 raise SyntaxError
-                # add something to kill everything
-                # raise SyntaxError will kill everything and tell the user there is a syntax error
-                pass
 
+        #parses variables
         extracted_strings = re.split(r",\s*(?![^()]*\))",extracted_string)#vodo magic that dosn't split on commas between parentheses
-        extracted_strings[0] = extracted_strings[0].split(r'=')
-        extracted_strings[0] = extracted_strings[0][0]
+        extracted_strings[0] = extracted_strings[0].split(r'=') #deals with x=x
+        try:
+            extracted_strings[0] = extracted_strings[0][0] #removes string within string, try stops python from dying...
         
         # creates a dictionary out of the extracted string
         var_dictionary = {}
@@ -126,22 +101,19 @@ def function_finder(equ, debug=False):
         variable_dictionary_list.append(var_dictionary)
 
     if debug is True:
-        # TODO Make debug here much more detailed printout
         print("DEBUG: Functions.py")
         print("Input string: " + equ)
-        print(function_ranges)
-        print(fun_list)
-        print(fun_list1)
-        print("\nOutput: \n")
-    # return values
+        print('functions ranges: ' + function_ranges)
+        print('start list: ' + start_list)
+        print('parentheses ranges: ' + parentheses_ranges)
+        print('function ranges: ' + function_ranges)
+        print("\nOutput: ")
+    
     return fun_list, variable_dictionary_list
 
 
 if __name__ == "__main__":
     print("\nTesting Functions.py.......\n")
-    # I jumped down the rabbit hole of test cases but I want to
-    # be sure on what you are looking for before going crazy
-    # so I just did something simple
     equ = 'x=2*5+function1(( x=x, 5/4, 12^(2-1)), 8) * function2(y, 7, 11*(y-3))'
     print(function_finder(equ, debug=True))
 
