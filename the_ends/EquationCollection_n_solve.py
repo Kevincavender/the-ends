1fd96e895616cf7ea511747374e0bb3ba99b5809
severnaa <44298139+severnaa@users.noreply.github.com>
2018-12-13 18:03:10 -0700

    Update EquationCollection_n_solve.py

--- the_ends/EquationCollection_n_solve.py
+++ the_ends/EquationCollection_n_solve.py
@@ -127,70 +127,25 @@ class EquationCollection_n_solve(object):
         else:
             raise TypeError
 
-    def parse_line(self, line_string, line_number):
-        line = EquationErrorCheck(line_string, line_number)
-        try:
-            line.checkline()
-        except SyntaxError:
-            print("Syntax Error Detected:\n\tLine error")
-        equations = line_string.split("\n")
-        equations = equations.replace(' ', '')
-        return equations
-
     """
     def update_solved_variable_list(self):
         for i in self.variables:
             if self.variables[i]['solved'] is True:
                 self.solved_variable_list.append(self.variables[i]['variable_name'])
                 self.solved_variable_list = list(set(self.solved_variable_list))
-    """
+    
     
     def update_solved_equation_list(self):
         for i in self.equations:
             if self.equations[i]['solved'] is True:
                 self.solved_equation_list.append(self.equations[i]['equation'])
                 self.solved_equation_list = list(set(self.solved_equation_list))
-
-    def update_list_from_dictionary(self, dictionary_name: dict, key_name: str, list_name: list) -> list:
-        # function to update a list taking a key from the variable or equation dictionary
-        # this is intended to ignore duplicates
-        for i in dictionary_name:
-            if dictionary_name[i][key_name] not in list_name:
-                list_name.append(dictionary_name[i][key_name])
-        list_name_output = list(set(list_name))
-        return list_name_output
+    """
 
     def separate_functions(self, equation=False):
         # TODO integrate function separation here
         pass
 
-    def separate_variables(self, equation=False):
-        """
-        split equations by the all known operators
-        store in list for processing
-        :param equation:
-        :return:
-            list of variables (without copies)
-        """
-        variables = []
-
-        if equation is False:
-            for k in self.equations:
-                # regular expression for splitting strings with given characters
-                split_equations = re.split(r"[=+\-^*/\\()\[\]]", self.equations[k]['equation'])
-                for i in split_equations:
-                    if self.is_variable(i) and i not in variables:
-                        variables.append(i)
-            return variables
-        elif isinstance(equation, str): #for outside calls
-            equation = equation.replace(' ','')
-            split_equations = re.split(r"[=+\-^*/\\()\[\]]", equation)
-            for i in split_equations:
-                if self.is_variable(i) and i not in variables:
-                    variables.append(i)
-            return variables
-        else:
-            raise TypeError
 
     def is_variable(self, variable):
         if variable is '':
@@ -212,14 +167,11 @@ class EquationCollection_n_solve(object):
         print("------------------------------------------")
         print("--------------DEBUG-PRINTOUT--------------")
         print("------------------------------------------")
-        pprint(self.master, depth=1)
-        print("\n\n")
-        pprint(self.master)
+        pass
 
 
 
 if __name__ == "__main__":
-    EQ = EquationCollection("x=1\ny=2\n\na= x+y\nsqu=sqa(")
+    EQ = EquationCollection_n_solve("x=1\ny=2\n\na= x+y\nsqu=sqa(")
     EQ.add_equation_to_dictionary("words=1", 4)
-    EQ.update_class()
     EQ.debug_output()
